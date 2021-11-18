3aa8cdc8db520485857653d1a9148eefd9a8ce58
RomanovYO <94523600+RomanovYO@users.noreply.github.com>
2021-11-18 15:26:51 +0200

    Создание основы программы

---
+++ csvAnalyst.py
@@ -0,0 +1,11 @@
+﻿# программа анализа .csv файлов
+
+import tkinter as tk
+
+# Создание главного окна
+window = tk.Tk()
+window.geometry('550x550')
+window.title('csvAnalyst - программа анализа .csv файлов')
+
+# Запуск цикла mainloop
+window.mainloop()
